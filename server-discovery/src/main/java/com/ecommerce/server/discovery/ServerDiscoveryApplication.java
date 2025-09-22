68b386cfc957a0505733bcfb42a6f6a9081719a9
Anthony Puitiza Lopez <anthony.puitiza.02@gmail.com>
2025-09-21 19:03:39 -0500

    remove Spring Boot Admin Server

--- server-discovery/src/main/java/com/ecommerce/server/discovery/ServerDiscoveryApplication.java
+++ server-discovery/src/main/java/com/ecommerce/server/discovery/ServerDiscoveryApplication.java
@@ -1,12 +1,10 @@
 package com.ecommerce.server.discovery;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
-@EnableAdminServer
 @SpringBootApplication
 public class ServerDiscoveryApplication {
 
